9242674245d604ec47037b6e6f65273b8ca26bb3
anmoltiwari0225 <anmoltiwari0225@gmail.com>
2022-09-11 04:06:05 +0530

    Solution to the qr-code challenge

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '375px',
+      xl: '1440px',
+    },
+    colors: {
+      'white': 'hsl(0, 0%, 100%)',
+      'light-gray': 'hsl(212, 45%, 89%)',
+      'grayish-blue': 'hsl(220, 15%, 55%)',
+      'dark-blue': 'hsl(218, 44%, 22%)',
+    },
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif'],
+    },
+    fontSize: {
+      'base': '15px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
